6a0ae20a701b574630aaf102b8bf1bb0a9117f4b
Itai Ben David <itai.benda@gmail.com>
2017-05-29 16:22:45 +0300

    separate navigation link from its layout & use only the layout for drill view

--- src/SideMenu/DrillView/Link.js
+++ src/SideMenu/DrillView/Link.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {node, bool, string} from 'prop-types';
 import SideMenu from '../index';
 
-const Link = ({children, to, isActive, ...rest}) => (
-  <SideMenu.NavigationLink href={to} isActive={isActive} {...rest}>
+const Link = ({children, isActive, ...rest}) => (
+  <SideMenu.NavigationLinkLayout isActive={isActive} {...rest}>
     {children}
-  </SideMenu.NavigationLink>
+  </SideMenu.NavigationLinkLayout>
 );
 
 Link.defaultProps = {
@@ -13,7 +13,6 @@ Link.defaultProps = {
 };
 
 Link.propTypes = {
-  to: string.isRequired,
   children: node.isRequired,
   isActive: bool
 };
